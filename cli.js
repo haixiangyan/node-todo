edde4cd16d4ac46ff76f0d0cf0b5064f258bbbde
Haixiang <haixiang6123@gmail.com>
2020-03-06 17:45:55 -0800

    Add version

--- cli.js
+++ cli.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 const program = require('commander')
 const api = require('./index')
+const pkg = require('./package.json')
+
+program.version(pkg.version)
 
 program
     .command('add')
