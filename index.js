3687552195d6ef5c67fb201217274958ff2038fd
Haixiang <haixiang6123@gmail.com>
2020-03-06 17:17:35 -0800

    Add inquirer

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const inquirer = require('inquirer')
 const db = require('./db')
 
 module.exports.add = async (title) => {
@@ -19,5 +20,77 @@ module.exports.showAll = async () => {
     // 打印之前的任务
     list.forEach((task, index) => {
         console.log(`${task.done ? '[X]' : '[_]'} ${index + 1} - ${task.title}`)
+    })
+
+    inquirer.prompt([{
+        type: 'list',
+        name: 'index',
+        message: '请选择你想操作的任务',
+        choices: [
+            {value: '-1', name: '[+] 创建任务'},
+            ...list.map((task, index) => {
+                return {
+                    value: index.toString(),
+                    name: `${task.done ? '[X]' : '[_]'} ${index + 1} - ${task.title}`
+                }
+            }),
+            {value: '-2', name: '[<- 退出'}
+        ]
+    }]).then(answer => {
+        const index = parseInt(answer.index)
+        if (index >= 0) {
+            inquirer.prompt({
+                type: 'list',
+                name: 'action',
+                message: '请选择操作',
+                choices: [
+                    {value: 'quit', name: '退出'},
+                    {value: 'done', name: '已完成'},
+                    {value: 'undone', name: '未完成'},
+                    {value: 'remove', name: '删除'},
+                    {value: 'update', name: '修改'}
+                ]
+            }).then(answer => {
+                switch (answer.action) {
+                    case 'quit':
+                        break
+                    case 'done':
+                        list[index].done = true
+                        db.write(list)
+                        break
+                    case 'undone':
+                        list[index].done = false
+                        db.write(list)
+                        break
+                    case 'update':
+                        inquirer.prompt({
+                            type: 'input',
+                            name: 'title',
+                            message: '新的任务标题',
+                            default: list[index].title
+                        }).then(answer => {
+                            list[index].title = answer.title
+                            db.write(list)
+                        })
+                        break
+                    case 'remove':
+                        list.splice(index, 1)
+                        db.write(list)
+                        break
+                }
+            })
+        } else if (index === -1) {
+            inquirer.prompt({
+                type: 'input',
+                name: 'title',
+                message: '输入任务标题',
+            }).then(answer => {
+                list.push({
+                    title: answer.title,
+                    done: false
+                })
+                db.write(list)
+            })
+        }
     })
 }
