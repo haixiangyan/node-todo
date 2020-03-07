ae19ceb92767fd254df93c79e0bcb39ba89324e3
Haixiang <haixiang6123@gmail.com>
2020-03-06 16:44:40 -0800

    Show all tasks

--- cli.js
+++ cli.js
@@ -2,19 +2,23 @@ const program = require('commander')
 const api = require('./index')
 
 program
-    .option('-d, --debug', 'output extra debugging')
-program
     .command('add')
     .description('添加一个任务')
-    .action((...args) => {
+    .action(async (...args) => {
         api.add(args[1].join(' '))
+            .then(() => '添加成功', () => '添加失败')
     })
 
 program
     .command('clear')
     .description('清除所有任务')
-    .action((...args) => {
-        console.log('This is clear')
+    .action(async () => {
+        await api.clear()
+            .then(() => '清除完毕', () => '清除失败')
     })
 
 program.parse(process.argv)
+
+if (process.argv.length === 2) {
+    void api.showAll()
+}
